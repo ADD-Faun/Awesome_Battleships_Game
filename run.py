d00308005dd5c6d33ec171f719c3d31e27f78358
ADD-Faun <adammbus@hotmail.com>
2022-12-06 16:30:23 +0000

    named needed functions and saved them

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+# takes user input of row & col 
+def user_guess():
+    pass
+
+# chooses a position  to place ship
+def place_ship():
+    pass
+
+# checks ship placement is avalible
+def validate_place_ship():
+    pass
+
+# checks if guess is a hit or miss to update board 
+def hit_miss():
+    pass
+
+# checks if either side has lost all their ships
+def finish_game():
+    pass
